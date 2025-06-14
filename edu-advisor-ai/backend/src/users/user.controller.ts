908c26fce87e104f3627511e29e6d3b43a1eb292
rustem-garr <rustem.garr@gmail.com>
2025-06-13 21:26:37 -0500

    Implemented user login controller

--- edu-advisor-ai/backend/src/users/user.controller.ts
+++ edu-advisor-ai/backend/src/users/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from 'bcryptjs';
 import { UserModel } from "./user.model";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req:Request, res:Response) => {
     try{
@@ -27,6 +28,38 @@ export const registerUser = async (req:Request, res:Response) => {
         }
         else{
             res.status(500).json({message:'unknown error occured'});
+        }
+    }
+}
+
+export const loginUser = async (req:Request, res:Response) => {
+    try{
+        const {email, password} = req.body;
+        const user = await UserModel.findOne({email});
+        if(!user){
+            res.status(401).json({message:'Invalid email'});
+        }
+        const accessToken = jwt.sign(
+            {userId: user!._id},
+            process.env.ACCESS_TOKEN_SECRET!,
+            {expiresIn: '15m'}
+        );
+
+        const refreshToken = jwt.sign(
+            {userId:user!._id},
+            process.env.REFRESH_TOKEN_SECRET!,
+            {expiresIn:'7d'}
+        );
+
+        res.status(200).json({accessToken, refreshToken});
+
+    }
+    catch(error){
+        if (error instanceof Error){
+            res.status(500).json({message:error.message});
+        }
+        else{
+            res.status(500).json({message:'Unknown error occurred'});
         }
     }
 }
