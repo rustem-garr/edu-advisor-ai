3c6adbbb949bae020bbfaa97d29ca36c6f815cbf
rustem-garr <rustem.garr@gmail.com>
2025-06-14 15:07:05 -0500

    Created roadmapSchema, embedded to userSchema and created learningResourceModel

--- edu-advisor-ai/backend/src/users/user.model.ts
+++ edu-advisor-ai/backend/src/users/user.model.ts
@@ -1,13 +1,60 @@
-import {Schema, model} from 'mongoose';
+import {Schema, model, Types} from 'mongoose';
+
+const resourceSchema = new Schema({
+    type: String,
+    title: String,
+    url: String,
+}); 
+
+const stepSchema = new Schema({
+    stepNumber: Number,
+    title: String,
+    description: String,
+    resources: [resourceSchema],
+    isCompleted: { type: Boolean, default: false },
+});
+
+const roadmapSchema = new Schema({
+    topic: { type: String, required: true },
+    userInput: {
+        experienceLevel: { type: String, required: true },
+        learningStyle: { type: String, required: true },
+    },
+    steps: [stepSchema],
+}, {
+    timestamps: true 
+});
+ 
 
 const userSchema = new Schema({
-    email: {type:String, required:true, unique:true, trim:true, lowercase:true},
-    password: {type:String, required:true}
-}, {timestamps:true});
-
-export type User = {
-    email:string,
-    password:string
-}
+    email: { type: String, unique: true, required: true, trim:true, lowercase:true },
+    password: { type: String, required: true },
+    roadmaps: [roadmapSchema] 
+}, {
+    timestamps: true
+});
 
 export const UserModel =  model("User", userSchema);
+
+
+export interface LearningResource {
+    _id: Types.ObjectId,
+    source: string, 
+    topic:string, 
+    chunkText: string,
+    embedding:number[]
+}
+
+const learningResourceSchema = new Schema<LearningResource>({
+    source:{type:String, required:true},
+    topic:{type:String, required:true, index:true},
+    chunkText:{type:String, required:true},
+    embedding: {
+        type:[Number],
+        required:true,
+    },
+},{
+    timestamps:true
+});
+
+export const LearningResourceModel = model<LearningResource>('LearningResource', learningResourceSchema);
