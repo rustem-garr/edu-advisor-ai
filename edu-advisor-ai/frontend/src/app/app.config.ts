e781254a3ca05d5b42316a14ed99e715c3b0a5b3
rustem-garr <rustem.garr@gmail.com>
2025-06-15 15:31:41 -0500

    added angular material and configured HttpClient for backend communication

--- edu-advisor-ai/frontend/src/app/app.config.ts
+++ edu-advisor-ai/frontend/src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi())
   ]
 };
