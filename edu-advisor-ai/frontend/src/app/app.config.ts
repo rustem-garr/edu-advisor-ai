15b17922ff3a8e05920408e3344c5b025d932398
rustem-garr <rustem.garr@gmail.com>
2025-06-15 18:06:30 -0500

    configured HttpClient for backend communication

--- edu-advisor-ai/frontend/src/app/app.config.ts
+++ edu-advisor-ai/frontend/src/app/app.config.ts
@@ -2,11 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()) 
   ]
 };
