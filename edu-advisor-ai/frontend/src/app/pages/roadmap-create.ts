4fc08e41f59c4f840ddade71bd8fbf572f20dc0b
rustem-garr <rustem.garr@gmail.com>
2025-06-16 15:11:13 -0500

    generated roadmap create and detail components

---
+++ edu-advisor-ai/frontend/src/app/pages/roadmap-create.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roadmap-create',
+  imports: [],
+  template: `
+    <p>
+      roadmap-create works!
+    </p>
+  `,
+  styles: ``
+})
+export class RoadmapCreate {
+
+}
---
+++ edu-advisor-ai/frontend/src/app/pages/roadmap-detail.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roadmap-detail',
+  imports: [],
+  template: `
+    <p>
+      roadmap-detail works!
+    </p>
+  `,
+  styles: ``
+})
+export class RoadmapDetail {
+
+}
