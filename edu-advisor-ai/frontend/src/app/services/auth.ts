bdd3860668a1cd5d519af33a2024da1164ee909e
rustem-garr <rustem.garr@gmail.com>
2025-06-15 20:02:54 -0500

    updated auth service to use localStorage for session persistance

--- edu-advisor-ai/frontend/src/app/services/auth.ts
+++ edu-advisor-ai/frontend/src/app/services/auth.ts
@@ -1,28 +1,60 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, computed, inject, signal } from '@angular/core';
+import { Injectable, computed, effect, inject, signal } from '@angular/core';
 import { AuthResponse, User } from '../interfaces/user.interface';
 import {tap} from 'rxjs';
+import { Router } from '@angular/router';
+
+const USER_KEY = "current_user";
+const TOKEN_KEY = "auth_token";
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
   #http = inject(HttpClient);
+  #router = inject(Router);
   #baseUrl = 'http://localhost:3000/api/users';
 
-  currentUser = signal<User | null | undefined>(undefined);
-  isAuthenticated = computed(()=>!this.currentUser());
+  currentUser = signal<User | null | undefined>(this.#getStoredUser());
+  isAuthenticated = computed(()=>!!this.currentUser());
+
+  authEffect = effect(()=>{
+    if(this.isAuthenticated()){
+      this.#router.navigate(['/dashboard']);
+    }
+    else{
+      this.#router.navigate(['/login']);
+    }
+  });
 
   login(credentials: Partial<{email:string, password:string}>){
     return this.#http.post<AuthResponse>(`${this.#baseUrl}/login`, credentials).pipe(
       tap(response=>{
-        console.log('Access token:', response.accessToken);
+        localStorage.setItem(TOKEN_KEY, response.accessToken);
+        const user: User = {_id: 'temp_id', email: credentials.email!};
+        localStorage.setItem(USER_KEY, JSON.stringify(user));
+        this.currentUser.set(user);
       })
     )
   }
 
+  logout(){
+    localStorage.removeItem(TOKEN_KEY);
+    localStorage.removeItem(USER_KEY);
+    this.currentUser.set(null);
+  }
+
   register(credentials:Partial<{email:string, password:string}>){
     return this.#http.post<{message:string}>(`${this.#baseUrl}/register`, credentials);
+  }
+
+  getToken():string|null{
+    return localStorage.getItem(TOKEN_KEY);
+  }
+
+  #getStoredUser():User|null{
+    const user = localStorage.getItem(USER_KEY);
+    return user ? JSON.parse(user):null;
   }
 
   constructor() { }
