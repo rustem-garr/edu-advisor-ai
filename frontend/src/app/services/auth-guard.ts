3faff75600f05c07ee847dc57c460afaaa46b9cb
rustem-garr <rustem.garr@gmail.com>
2025-06-17 02:19:39 -0500

    updated project structure and added readme

--- frontend/src/app/services/auth-guard.ts
+++ frontend/src/app/services/auth-guard.ts
@@ -1,6 +1,6 @@
 import { CanActivateFn, Router } from '@angular/router';
 import {inject} from '@angular/core';
-import {Auth} from '../services/auth';
+import {Auth} from './auth';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(Auth);
